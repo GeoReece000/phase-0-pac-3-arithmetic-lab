65910f102c33a065971f69319f05c198be8f59a6
GeoReece000 <geofreytegeret@gmail.com>
2023-03-10 10:36:04 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(a, b) {
+    return a+b
+    }
+add(1,2);
+
+function subtract(a,b) {
+    return a - b
+    }
+subtract(2,1);
+
+function multiply(a,b) {
+   return  a * b
+}
+multiply(2,2);
+
+function divide(a,b) {
+    return a/b
+}
+divide(4/2);
+
+function increment(n) {
+    return n + 1;
+}
+increment(397);
+
+function decrement(n) {
+    return n -1
+}
+
